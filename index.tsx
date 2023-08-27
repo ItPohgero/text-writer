86689fa0bc454c24d5cb3bd7ceecfd53394d3833
wahyuagusarifin <wahyu.agusarifin@ai.astra.co.id>
2023-08-27 11:57:22 +0700

    version 1

---
+++ index.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+interface DataProps {
+    text: string | string[]
+    delay?: number
+    isLoop?: boolean
+    loopDelay?: number
+    textClassName?: string
+}
+
+const Typewriter = ({ text, delay = 100, isLoop = false, loopDelay = 2000, textClassName = 'text-slate-700' }: DataProps) => {
+    const [currentText, setCurrentText] = useState('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const textArray = Array.isArray(text) ? text : [text];
+        const currentTextLength = currentText.length;
+
+        if (currentIndex < textArray.length) {
+            if (currentTextLength < textArray[currentIndex].length) {
+                const timeout = setTimeout(() => {
+                    setCurrentText(prevText => prevText + textArray[currentIndex][currentTextLength]);
+                }, delay);
+
+                return () => clearTimeout(timeout);
+            } else if (currentIndex === textArray.length - 1 && isLoop) {
+                setTimeout(() => {
+                    setCurrentText('');
+                    setCurrentIndex(0);
+                }, loopDelay);
+            } else {
+                setCurrentIndex(prevIndex => prevIndex + 1);
+                setCurrentText('');
+            }
+        }
+    }, [currentIndex, currentText, delay, isLoop, loopDelay, text]);
+
+    return <span className={textClassName}>{currentText}</span>;
+};
+
+export default Typewriter;
