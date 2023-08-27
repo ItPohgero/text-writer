52f2f3461940bc70896bba27c2e605f006479a9a
itpohgero <itpohgero@gmail.com>
2023-08-27 12:51:20 +0700

    108

--- Typewriter.tsx
+++ Typewriter.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 const Typewriter = ({ text, delay = 100, isLoop = false, loopDelay = 2000, textClassName = 'text-slate-700' }: {
-    text: string | string[];
-    delay?: number;
-    isLoop?: boolean;
-    loopDelay?: number;
-    textClassName?: string;
+    text: string | string[],
+    delay?: number,
+    isLoop?: boolean,
+    loopDelay?: number,
+    textClassName?: string,
 }) => {
     const [currentText, setCurrentText] = useState('');
     const [currentIndex, setCurrentIndex] = useState(0);
